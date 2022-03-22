3c5650c11e20d50ffde043db4f95430c4bc8aeba
Volker Hoffmann <volker.hoffmann@sintef.no>
2022-03-22 15:47:21 +0100

    add partitioned config to job

--- vesselai_dagster/jobs/download_stuff.py
+++ vesselai_dagster/jobs/download_stuff.py
@@ -1,8 +1,20 @@
 from dagster import job
+from dagster import daily_partitioned_config
+from datetime import datetime
 
 from vesselai_dagster.ops.download import download_nwp
 
 
+@daily_partitioned_config(start_date=datetime(2019, 1, 1), end_offset=-1160)
+def my_partitioned_config(start: datetime, _end: datetime):
+    return {
+        "ops": {
+            "process_data_for_date": {"config": {"date": start.strftime("%Y-%m-%d")}}
+        }
+    }
+
+
+@job(config=my_partitioned_config)
 def download_stuff_job():
     """
     A job definition. This example job has a single op.
